66b0498cefa64e0414f3fdf65a66ae877538a29a
willjthomas <135294318+willjthomas@users.noreply.github.com>
2023-08-06 20:09:06 -0400

    edited capitalization for consistency

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,10 +2,10 @@
 for (i) in range(1, 101):
     if i % 3 == 0:
         if i % 5 == 0:
-            print("FIZZBUZZ")
+            print("FizzBuzz")
         else:
-            print("FIZZ")
+            print("Fizz")
     elif i % 5 == 0:
-        print("BUZZ") 
+        print("Buzz") 
     else:
         print(i)
